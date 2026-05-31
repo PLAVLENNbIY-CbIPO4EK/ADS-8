25f98bfb334683b8ae28b714a49cc18bd49a2556
PLAVLENNbIY-CbIPO4EK <ilya.potor2713@gmail.com>
2026-05-31 14:23:32 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
+#include <iostream>
 #include <string>
 
-void makeTree(BST<std::string>& tree,
-              const char* filename);
-
-void printFreq(BST<std::string>& tree);
+#include "bst.h"
+#include "alg.h"
 
 int main() {
     BST<std::string> tree;
 
-    makeTree(tree, "src/war_peace.txt");
+    makeTree(tree, "war_peace.txt");
+
+    std::cout << tree.depth() << std::endl;
 
     printFreq(tree);
 
